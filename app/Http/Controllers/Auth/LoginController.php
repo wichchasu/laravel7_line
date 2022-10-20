b8c2a1454d0c2e6c1b4e3b69988a6f8dc4046e5b
wichchasu <wichcha.su@bsru.ac.th>
2022-10-20 16:50:19 +0700

    first

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\MyAuthProvider;
+use App\Providers\RouteServiceProvider;
+use App\User;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+
+//use Auth;
+use App\UserLoginLog;
+use App\Events\UserEvent;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
+use Laravel\Socialite\Facades\Socialite;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    function authenticated(Request $request, $user)
+    {
+        if (!Auth::check()) {
+            return view('errors.404');
+        }
+        event(new UserEvent($request, $user));
+    }
+
+//    public function login(Request $request){
+//        /**
+//         *
+//         *
+//         *
+//         */
+//
+//        $request->validate(
+//            [
+//                'email'=>['required'],
+//                'password'=>['required']
+//            ]
+//
+//        );
+//
+////        if($request->has(['password']))
+//
+//
+//        $credentials = $request->only('email', 'password');
+//        //$user = User::where('email',$request->email)->where('password',Hash::make($request->password))->first();
+//        if(\Illuminate\Support\Facades\Auth::attempt($credentials))
+//            //\Illuminate\Support\Facades\Auth::login($user);
+//            return redirect()->route('home');
+//        else
+//            throw ValidationException::withMessages([
+//                'email'=>["Username or password is incorrect"]
+//            ]);
+//        return redirect()->route('home');
+//    }
+
+    public function redirectToLine()
+    {
+
+        return Socialite::driver('line')->redirect();
+    }
+
+    public function handleLineCallback()
+    {
+        try {
+            $user = Socialite::driver('line')->user();
+
+            $finduser = MyAuthProvider::where('provider', 'line')->where('provider_id', $user->id)->first();
+            if ($finduser) {
+                $user = User::where('id', $finduser->user_id)->first();
+                Auth::login($user);
+
+                return redirect('/');
+            } else {
+                $newUser = new User();
+                $newUser->name = $user->name ? $user->name : $user->nickname;
+                $newUser->email = $user->email;
+                $newUser->save();
+                $newUser->assignRole('Member');
+
+                $new_user = new MyAuthProvider();
+                $new_user->user_id = $newUser->id;
+                $new_user->provider = 'line';
+                $new_user->provider_id = $user->id;
+                $new_user->save();
+                Auth::login($newUser);
+                return redirect('/');
+            }
+        } catch (\Exception $e) {
+
+            Log::error($e->getMessage());
+            return redirect('/');
+        }
+    }
+
+}
---
+++ app/Http/Controllers/MyAuthProviderController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\MyAuthProvider;
+use Illuminate\Http\Request;
+
+class MyAuthProviderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\MyAuthProvider  $myAuthProvider
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MyAuthProvider $myAuthProvider)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\MyAuthProvider  $myAuthProvider
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MyAuthProvider $myAuthProvider)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\MyAuthProvider  $myAuthProvider
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MyAuthProvider $myAuthProvider)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\MyAuthProvider  $myAuthProvider
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MyAuthProvider $myAuthProvider)
+    {
+        //
+    }
+}
